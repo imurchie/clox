99430e84c897c9be8b5a46f8f984731006ba20bd
Isaac A. Murchie <murchieisaac@gmail.com>
2024-08-26 10:43:39 -0400

    feat: add basic binary operators (#6)

--- main.c
+++ main.c
@@ -12,9 +12,22 @@ int main(int argc, const char* argv[]) {
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
 
-  writeChunk(&chunk, OP_RETURN, 124);
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 124);
+  writeChunk(&chunk, constant, 124);
+
+  writeChunk(&chunk, OP_ADD, 124);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 124);
+  writeChunk(&chunk, constant, 124);
+
+  writeChunk(&chunk, OP_DIVIDE, 124);
+
+  writeChunk(&chunk, OP_NEGATE, 124);
+
+  writeChunk(&chunk, OP_RETURN, 126);
 
   // for debugging purposes
   // disassembleChunk(&chunk, "test chunk");
--- vm.c
+++ vm.c
@@ -43,6 +43,13 @@ InterpretResult interpret(Chunk* chunk) {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
+
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
@@ -56,9 +63,21 @@ static InterpretResult run() {
 #endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
+      case OP_ADD: {
+        BINARY_OP(+);
+        break;
+      }
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
         push(constant);
+        break;
+      }
+      case OP_DIVIDE: {
+        BINARY_OP(/);
+        break;
+      }
+      case OP_MULTIPLY: {
+        BINARY_OP(*);
         break;
       }
       case OP_NEGATE: {
@@ -70,8 +89,14 @@ static InterpretResult run() {
         printf("\n");
         return INTERPRET_OK;
       }
+      case OP_SUBTRACT: {
+        BINARY_OP(-);
+        break;
+      }
     }
   }
-#undef READ_CONSTANT
+
 #undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
 }
